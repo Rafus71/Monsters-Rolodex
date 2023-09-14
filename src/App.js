f58ca6b418f7f11d9d25f1c44496676305429451
Rafus71 <rulanski71@gmail.com>
2023-09-14 20:59:27 +0100

    Start project: app.js - clean previous tests implementate  monsters state array

--- src/App.js
+++ src/App.js
@@ -8,8 +8,22 @@ class App extends Component {
     super();
     
     this.state = {
-      name: {firstName: 'Rafi', lastName:'Ulanski'},
-      company: 'CryptoRafus'
+      monsters: [
+         {
+          name: 'Linda'
+        },
+         {
+          name: 'Frank'
+        },
+         {
+          name: 'Jacky'
+        },
+        {
+          name: 'Andrew'
+        },
+        
+      ]
+      
       
     }
   }
@@ -17,22 +31,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-                Hello {this.state.name.firstName} {this.state.name.lastName} , I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(() => {
-              return{
-                name: { firstName: 'Sylwia' , lastName: 'Loboda'},
-              }
-            }, () => {
-              console.log(this.state);
-            });
-              
-          }}>Change Name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
       </div>
     );
 
