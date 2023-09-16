eb3e3bd5bc6d349cefc74095b9e653ef46a2c4a7
Rafus71 <rulanski71@gmail.com>
2023-09-16 11:43:45 +0100

    added input search component, searching and filtering

--- src/App.js
+++ src/App.js
@@ -34,6 +34,15 @@ class App extends Component {
       <div className="App">
         <input className=',search box' type='search' placeholder='search monsters' onChange={(event) => {
           console.log(event.target.value);
+          const searchString = event.target.value.toLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => { return monster.name.toLowerCase().includes(searchString);
+            
+          });
+          
+          this.setState(() => {
+            return {monsters: filteredMonsters}
+          })
+
         }}/>
           
         {this.state.monsters.map((monster) => {
